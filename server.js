7126f070a6d8876164a91aeb047359b34923c09c
kumarrahulsindhu2004 <kumarrahulsindhu@gmail.com>
2025-10-05 21:54:03 +0530

    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://superlative-sorbet-c275a8.netlify.app"
+    "https://mockpractice2025.netlify.app/"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
