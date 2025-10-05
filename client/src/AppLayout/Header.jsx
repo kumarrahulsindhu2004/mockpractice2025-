d73b2c27da1db9afb75dfaad03eb21b9e55586b4
kumarrahulsindhu2004 <kumarrahulsindhu@gmail.com>
2025-10-05 10:19:48 +0530

    updated

--- client/src/AppLayout/Header.jsx
+++ client/src/AppLayout/Header.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import { Link, useLocation, useNavigate } from "react-router-dom"
 import axios from "axios"
-import "./header.css"
+import "./Header.css"
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false)
